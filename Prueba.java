1ef49dee5cc9d867b578fd32c1eaa83549c2cd41
sabraro <sabraro95@gmail.com>
2021-10-15 12:09:36 +0200

    Residencia,SO

--- Prueba.java
+++ Prueba.java
@@ -2,5 +2,6 @@ public class Prueba {
 	public static void main(String args[]) {
 		System.out.println("Sabina Bracic Rodriguez");
 		System.out.println("Leon,1995");
+		System.out.println("Leon,Windows");
 	}
 }
