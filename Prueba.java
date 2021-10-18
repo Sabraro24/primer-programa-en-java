a5c093fcfb0f2ad7ff87f82cf8788231dbea27b0
sabraro <sabraro95@gmail.com>
2021-10-18 14:09:09 +0200

    Grupo

--- Prueba.java
+++ Prueba.java
@@ -1,7 +1,7 @@
 public class Prueba {
 	public static void main(String args[]) {
-		System.out.println("Sabina Bracic Rodriguez");
 		System.out.println("Leon,1995");
 		System.out.println("Leon,Windows");
+		System.out.println("U2");
 	}
 }
